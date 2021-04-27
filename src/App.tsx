10d32b19bd81f02b5a3e0367c07dd94031d925e7
Logan <logan@neutral.systems>
2021-04-27 13:20:05 +0100

    Changed Tab size

--- src/App.tsx
+++ src/App.tsx
@@ -11,69 +11,69 @@ interface IAppProps {
 }
 
 interface IAppState {
-  Json: DBML;
-  HasData: boolean;
+    Json: DBML;
+    HasData: boolean;
 }
 
 export default class App extends React.Component<IAppProps, IAppState> {
-  static displayName = App.name;
+    static displayName = App.name;
 
-  constructor(props: any) {
-    super(props);
-    this.state={
-      Json: {} as DBML,
-      HasData: false,
-    };
-  }
-
-  componentDidMount() {
-    this.GetData();
-  }
-
-  public async GetData() {
-    const Data = await fetch('/Database/Example.dbml')
-    .then((r) => r.text())
-    .then(text  => {
-      return text.toString();
-    });
-
-    const Json = DBML2JSON(Data);
-
-    this.setState({
-      Json: Json,
-      HasData: true,
-    })
-  }
-
-  render () {
-    if (this.state.HasData === false) {
-      return(
-        <h1>No Data</h1>
-      );
+    constructor(props: any) {
+        super(props);
+        this.state={
+            Json: {} as DBML,
+            HasData: false,
+        };
     }
 
-    return (
-      <Layout Tables={this.state.Json.Tables}>
-        <Route exact path='/'>
-          <Database
-            Project={this.state.Json.Project}
-            DBType={this.state.Json.database_type}
-            Tables={this.state.Json.Tables}
-            Note={this.state.Json.Note}
-          />
-        </Route>
-        {this.state.Json.Tables.map(table => {
-          return(
-            <Route path={"/" + table.Name}>
-              <Table
-                Name={table.Name}
-                Columns={table.Columns}
-                Note={table.Note}
-              />
-            </Route>
-          );
-        })}
-      </Layout>
-    );
-  }
+    componentDidMount() {
+        this.GetData();
+    }
+
+    public async GetData() {
+        const Data = await fetch('/Database/Example.dbml')
+        .then((r) => r.text())
+        .then(text  => {
+            return text.toString();
+        });
+
+        const Json = DBML2JSON(Data);
+
+        this.setState({
+            Json: Json,
+            HasData: true,
+        })
+    }
+
+    render () {
+        if (this.state.HasData === false) {
+            return(
+                <h1>No Data</h1>
+            );
+        }
+
+        return (
+            <Layout Tables={this.state.Json.Tables}>
+                <Route exact path='/'>
+                    <Database
+                        Project={this.state.Json.Project}
+                        DBType={this.state.Json.database_type}
+                        Tables={this.state.Json.Tables}
+                        Note={this.state.Json.Note}
+                    />
+                </Route>
+                {this.state.Json.Tables.map(table => {
+                    return(
+                        <Route path={"/" + table.Name}>
+                            <Table
+                                Name={table.Name}
+                                Columns={table.Columns}
+                                Note={table.Note}
+                            />
+                        </Route>
+                    );
+                })}
+            </Layout>
+        );
+    }
 }
--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
+    <BrowserRouter basename={baseUrl}>
+        <App />
+    </BrowserRouter>,
+rootElement);
 
 registerServiceWorker();
