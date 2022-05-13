4aab4eee6c6129628079f38ad637a72c97218439
IllusiveBagel <hawkentroop@gmail.com>
2022-05-13 23:58:28 +0100

    Added Core Structure

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default Navigation;
---
+++ src/pages/Database.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Database = () => {
+    return (
+        <div></div>
+    );
+};
