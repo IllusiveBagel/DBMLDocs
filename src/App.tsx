1acbf8af68d5bbbd8c4a6a6a5343caf0c9892fce
IllusiveBagel <hawkentroop@gmail.com>
2022-05-13 23:47:13 +0100

    Started Refactor with fresh project

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,10 @@
-import config from "./config.json";
-import Database from "./Pages/Database";
-import Layout from "./Components/Layout";
 import React from "react";
-import Table from "./Pages/Table";
-import { DBML, Reference } from "./Lib/Declarations";
-import { DBML2JSON } from "./Lib/DBMLLib";
-import { Route } from "react-router";
+import "./App.css";
 
-interface IAppProps {
-
+function App() {
+  return (
+    <div>Test</div>
+  );
 }
 
-interface IAppState {
-    Json: DBML;
-    HasData: boolean;
-}
-
-export default class App extends React.Component<IAppProps, IAppState> {
-    static displayName = App.name;
-
-    constructor(props: any) {
-        super(props);
-        this.state={
-            Json: {} as DBML,
-            HasData: false,
-        };
-    }
-
-    componentDidMount() {
-        this.GetData();
-    }
-
-    public async GetData() {
-        const Data = await fetch(`/Database/${config.DatabaseName}.dbml`)
-        .then((r) => r.text())
-        .then(text  => {
-            return text.toString();
-        });
-
-        const Json = DBML2JSON(Data);
-
-        this.setState({
-            Json: Json,
-            HasData: true,
-        })
-    }
-
-    public GetReferences(tableName: string, References: Reference[]): Reference[] {
-        var refs: Reference[] = [];
-
-        if (References !== undefined) {
-            References.forEach(ref => {
-                if (ref.Primary.Table === tableName || ref.Secondary.Table === tableName) {
-                    refs.push(ref);
-                }
-            });
-        }
-
-        return refs;
-    }
-
-    render () {
-        if (this.state.HasData === false) {
-            return(
-                <h1>No Data</h1>
-            );
-        }
-
-        return (
-            <Layout Database={this.state.Json}>
-                <Route exact path='/'>
-                    <Database
-                        Project={this.state.Json.Project}
-                        DBType={this.state.Json.database_type}
-                        Tables={this.state.Json.Tables}
-                        Note={this.state.Json.Note}
-                        References={this.state.Json.References}
-                        GetReferences={this.GetReferences}
-                    />
-                </Route>
-                {this.state.Json.Tables.map((table, index) => {
-                    return(
-                        <Route path={"/" + table.Name} key={index}>
-                            <Table
-                                Name={table.Name}
-                                Alias={table.Alias}
-                                Columns={table.Columns}
-                                Note={table.Note}
-                                References={this.GetReferences(table.Name, this.state.Json.References)}
-                            />
-                        </Route>
-                    );
-                })}
-            </Layout>
-        );
-    }
-}
+export default App;
